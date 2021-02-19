edf8579273af02790356603ed957a39f0a0c69f1
Sajad Rahimi <sajadrahimi6633@gmail.com>
2021-02-19 12:36:29 +0330

    Add Detail Activity

---
+++ app/src/main/java/rahimi/app/retrofit/DetailBookActivity.java
@@ -0,0 +1,14 @@
+package rahimi.app.retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_book);
+    }
+}
