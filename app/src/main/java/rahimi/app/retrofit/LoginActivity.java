1702dc2bf5ed6e88e35a684452c8712d230e8ef9
Sajad Rahimi <sajadrahimi6633@gmail.com>
2021-02-20 15:42:33 +0330

    Register & Login Activity Designed

--- app/src/main/java/rahimi/app/retrofit/LoginActivity.java
+++ app/src/main/java/rahimi/app/retrofit/LoginActivity.java
@@ -2,13 +2,27 @@ package rahimi.app.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView txt_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txt_signup = findViewById(R.id.txt_signup);
+        txt_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/rahimi/app/retrofit/RegisterActivity.java
+++ app/src/main/java/rahimi/app/retrofit/RegisterActivity.java
@@ -2,13 +2,35 @@ package rahimi.app.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextView txt_login;
+
+    @Override
+  public void onBackPressed() {
+        startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+        finish();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txt_login = findViewById(R.id.txt_login);
+        txt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
+
+
     }
 }
